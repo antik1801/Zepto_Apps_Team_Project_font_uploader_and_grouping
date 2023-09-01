8a05521dd017586bd8734d8d623df8a5735e0922
Gazi Ehsanul Haque <antik.edu@gmail.com>
2023-09-01 08:31:36 +0600

    main

--- file-hunter-client/src/components/DragAndDrop.jsx
+++ file-hunter-client/src/components/DragAndDrop.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./DragAndDrop.css"
+
+
 
 const DragAndDrop = () => {
-  return (
-    <div>
-      Drag and Drop
-    </div>
-  )
-}
 
-export default DragAndDrop
+  const [files, setFiles] = useState([])
+  const [dragging, setDragging] = useState(false)
+
+  const handleDragEnter = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(true);
+  }
+  const handleDragLeave = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(false);
+  }
+  const handleDragOver = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  const handleDrop = e =>{
+    e.preventDefault();
+    e.stopPropagation();
+    setDragging(false);
+    const files = e.dataTransfer.files;
+    console.log(files)
+  }
+  
+  return (
+    <div className={`font-sans text-gray-900 border-box drag-drop-container  ${dragging ? 'dragging' : ''}`} onDragOver={handleDragOver} onDrop={handleDrop} onDragEnter={handleDragEnter}  onDragLeave={handleDragLeave}>
+      <div className="flex justify-center w-full mx-auto ">
+        <div className="flex flex-col items-center justify-center w-full h-auto my-20 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl">
+          <div className="mt-10 mb-10 text-center">
+            <h2 className="text-2xl font-semibold mb-2">Upload your files</h2>
+            <p className="text-xs text-gray-500">
+              File should be of format .mp4, .avi, .mov or .mkv
+            </p>
+          </div>
+          <form
+            action="#"
+            className="relative w-4/5 h-32  mb-10 bg-white bg-gray-100 rounded-lg shadow-inner"
+          >
+            <input type="file" id="file-upload" className="hidden" multiple onChange={(e)=>setFiles(e.target.files)}/>
+            <label
+              htmlFor="file-upload"
+              className="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer"
+            >
+              <p className="z-10 text-xs font-light text-center text-gray-500">
+                Drag & Drop your files here
+              </p>
+              <svg
+                className="z-10  h-8 text-indigo-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+              </svg>
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DragAndDrop;
--- file-hunter-client/src/components/Tables/CommonTable.jsx
+++ file-hunter-client/src/components/Tables/CommonTable.jsx
@@ -4,175 +4,175 @@ import Tabletitle from "../../mini-components/texts/Tabletitle";
 const CommonTable = () => {
   return (
     <div>
-      <section class=" antialiased   ">
-        <div class="h-full">
-          <div class="w-full  mx-auto shadow-lg rounded-sm border border-gray-200">
-            <header class="px-5 py-4 border-b border-gray-100">
+      <section className=" antialiased   ">
+        <div className="h-full">
+          <div className="w-full  mx-auto shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-100">
               <Tabletitle text="Fonts"/>
             </header>
-            <div class="p-3">
-              <div class="overflow-x-auto">
-                <table class="table-auto w-full">
-                  <thead class="text-xs font-semibold uppercase  ">
+            <div className="p-3">
+              <div className="overflow-x-auto">
+                <table className="table-auto w-full">
+                  <thead className="text-xs font-semibold uppercase  ">
                     <tr>
-                      <th class="p-2 whitespace-nowrap">
-                        <div class="font-semibold text-left">Name</div>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Name</div>
                       </th>
-                      <th class="p-2 whitespace-nowrap">
-                        <div class="font-semibold text-left">Email</div>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Email</div>
                       </th>
-                      <th class="p-2 whitespace-nowrap">
-                        <div class="font-semibold text-left">Spent</div>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">Spent</div>
                       </th>
-                      <th class="p-2 whitespace-nowrap">
-                        <div class="font-semibold text-center">Country</div>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-center">Country</div>
                       </th>
                     </tr>
                   </thead>
-                  <tbody class="text-sm divide-y divide-gray-100">
+                  <tbody className="text-sm divide-y divide-gray-100">
                     <tr>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="flex items-center">
-                          <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
                             <img
-                              class="rounded-full"
+                              className="rounded-full"
                               src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
                               width="40"
                               height="40"
                               alt="Alex Shatov"
                             />
                           </div>
-                          <div class="font-medium ">
+                          <div className="font-medium ">
                             Alex Shatov
                           </div>
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left">alexshatov@gmail.com</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">alexshatov@gmail.com</div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left font-medium text-green-500">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
                           $2,890.66
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-lg text-center">🇺🇸</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-lg text-center">🇺🇸</div>
                       </td>
                     </tr>
                     <tr>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="flex items-center">
-                          <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
                             <img
-                              class="rounded-full"
+                              className="rounded-full"
                               src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-06.jpg"
                               width="40"
                               height="40"
                               alt="Philip Harbach"
                             />
                           </div>
-                          <div class="font-medium ">
+                          <div className="font-medium ">
                             Philip Harbach
                           </div>
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left">philip.h@gmail.com</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">philip.h@gmail.com</div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left font-medium text-green-500">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
                           $2,767.04
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-lg text-center">🇩🇪</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-lg text-center">🇩🇪</div>
                       </td>
                     </tr>
                     <tr>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="flex items-center">
-                          <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
                             <img
-                              class="rounded-full"
+                              className="rounded-full"
                               src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-07.jpg"
                               width="40"
                               height="40"
                               alt="Mirko Fisuk"
                             />
                           </div>
-                          <div class="font-medium">
+                          <div className="font-medium">
                             Mirko Fisuk
                           </div>
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left">mirkofisuk@gmail.com</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">mirkofisuk@gmail.com</div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left font-medium text-green-500">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
                           $2,996.00
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-lg text-center">🇫🇷</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-lg text-center">🇫🇷</div>
                       </td>
                     </tr>
                     <tr>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="flex items-center">
-                          <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
                             <img
-                              class="rounded-full"
+                              className="rounded-full"
                               src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-08.jpg"
                               width="40"
                               height="40"
                               alt="Olga Semklo"
                             />
                           </div>
-                          <div class="font-medium ">
+                          <div className="font-medium ">
                             Olga Semklo
                           </div>
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left">olga.s@cool.design</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">olga.s@cool.design</div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left font-medium text-green-500">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
                           $1,220.66
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-lg text-center">🇮🇹</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-lg text-center">🇮🇹</div>
                       </td>
                     </tr>
                     <tr>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="flex items-center">
-                          <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
                             <img
-                              class="rounded-full"
+                              className="rounded-full"
                               src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-09.jpg"
                               width="40"
                               height="40"
                               alt="Burak Long"
                             />
                           </div>
-                          <div class="font-medium ">
+                          <div className="font-medium ">
                             Burak Long
                           </div>
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left">longburak@gmail.com</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">longburak@gmail.com</div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-left font-medium text-green-500">
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
                           $1,890.66
                         </div>
                       </td>
-                      <td class="p-2 whitespace-nowrap">
-                        <div class="text-lg text-center">🇬🇧</div>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-lg text-center">🇬🇧</div>
                       </td>
                     </tr>
                   </tbody>
